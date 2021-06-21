facd1ea96b9fcf06bb5244f0ecc6ff5391ed5cf6
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-21 16:09:48 +0100

    Added links for personal Linkedin, GitHub repository and Netlify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
      Weather App
-        
+     <Footer />   
     </div>
   );
 }

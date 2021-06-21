c7be8b4ec894960e9d554328188c5425e2101cb7
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-21 20:30:00 +0100

    added fake data for current weather and location

--- src/Weather.js
+++ src/Weather.js
@@ -13,10 +13,44 @@ export default function Weather() {
         <input className="form-submit" type="submit" value="Search" />
       </form>
       <div className="row">
-        <div className="col city">Lisbon</div>
+        <div className="col city">
+          {" "}
+          <h2>Lisbon </h2>{" "}
+        </div>
         <div className="col country">PT</div>
         <div className="col temp-conversion">
-          <a href="/" className="celsius-conv">°C</a> | <a href="/" className="fahrenheit-conv">°F</a>
+          <a href="/" className="celsius-conv">
+            °C
+          </a>{" "}
+          |{" "}
+          <a href="/" className="fahrenheit-conv">
+            °F
+          </a>
+        </div>
+      </div>
+      <div className="last-update">Last updated: Monday 21 June 20:13</div>
+      <div className="current-weather">
+        <div className="row">
+          <div className="col">image</div>
+          <div className="col">
+            <ul>
+              <li className="current-temp">
+                {" "}
+                <h1>24°C</h1>
+              </li>
+              <li className="current-description">Sunny</li>
+              <li className="feels-like">Feels like: 22°C</li>
+              <li>
+                {" "}
+                img <span className="current-max-temp">26</span>°C img{" "}
+                <span className="current-min-temp">17</span>°C
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">img Wind: 20 km/h</div>
+          <div className="col">img Humidity: 55%</div>
         </div>
       </div>
     </div>

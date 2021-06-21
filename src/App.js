a0a7edb5c2856025d6245d8f849ff335658e2e46
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-21 19:43:27 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-     Weather App
-     <Footer />   
+      <div className="container">
+        Weather App
+        <Footer />
+      </div>
     </div>
   );
 }

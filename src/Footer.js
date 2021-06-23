64ce6ce3d6291afeaa0e347b4234abf6778a6728
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-23 17:23:37 +0100

    styled footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,26 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-    return (
-      <div className="footer">
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/joana-vasconcelos-carvalheiro-569056a5/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Joana Vasconcelos
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/joanavjc/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://joanavc-weather-app-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          hosted on Netlify
-        </a>
-      </div>
-    );
+  return (
+    <div className="footer">
+      This project was coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/joana-vasconcelos-carvalheiro-569056a5/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Joana Vasconcelos
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/joanavjc/weather-app-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        open-sourced on GitHub
+      </a>{" "}
+    </div>
+  );
 }

cbecafbc35cb4cc01a7360281f6d8fcbda3f35b3
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-26 12:48:16 +0100

    created new component WeatherInfo to simplify main component Weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+
+import WeatherInfo from "./WeatherInfo";
 import location from "./media/location.png";
-import wind from "./media/wind.svg";
-import humidity from "./media/humidity.svg";
-import maxtemp from "./media/max-temp.png";
-import mintemp from "./media/min-temp.png";
+
 
 export default function Weather(props) {
   
@@ -24,7 +22,9 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed * 3.6,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      city: response.data.name,
+      country: response.data.sys.country,
     });
    
   }
@@ -41,69 +41,8 @@ export default function Weather(props) {
           <input className="form-submit" type="submit" value="Search" />
           <img className="location-img" src={location} alt="" />
         </form>
-        <div className="last-update">Last updated: <FormattedDate date={weather.date} /> </div>
-        <div className="row">
-          <div className="col-9 currently">
-            <h2 className="city">
-              Lisbon <span className="country">PT</span>
-            </h2>
-          </div>
-          <div className="col-3 temp-conversion">
-            <a href="/" className="celsius-conv">
-              °C
-            </a>{" "}
-            |{" "}
-            <a href="/" className="fahrenheit-conv">
-              °F
-            </a>{" "}
-          </div>
-        </div>
-        <div className="current-weather">
-          <div className="row">
-            <div className="col">
-              <img className="weather-icon" src={weather.icon} alt="" />
-            </div>
-            <div className="col">
-              <ul>
-                <li className="current-temp">
-                  <span className="temperature">
-                    {Math.round(weather.temperature)}
-                  </span>
-                  <span className="unit">°C</span>
-                </li>
-                <li className="current-description">{weather.description}</li>
-                <li className="feels-like">
-                  Feels like {Math.round(weather.feelsLike)}°C
-                </li>
-                <li className="max-min-temp">
-                  <img className="max-temp-img" src={maxtemp} alt="" />{" "}
-                  <span className="current-max-temp">
-                    {Math.round(weather.maxTemp)}
-                  </span>
-                  <span className="unit">°C</span>{" "}
-                  <img className="min-temp-img" src={mintemp} alt="" />{" "}
-                  <span className="current-min-temp">
-                    {Math.round(weather.minTemp)}
-                  </span>
-                  <span className="unit">°C</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="conditions">
-            <ul>
-              <li className="wind">
-                <img className="wind-img" src={wind} alt="" /> Wind:{" "}
-                {Math.round(weather.wind)} km/h
-              </li>
-              <li className="humidity">
-                {" "}
-                <img className="humidity-img" src={humidity} alt="" />
-                Humidity: {weather.humidity}%
-              </li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weather}/>
+        
       </div>
     );
   } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,81 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import wind from "./media/wind.svg";
+import humidity from "./media/humidity.svg";
+import maxtemp from "./media/max-temp.png";
+import mintemp from "./media/min-temp.png";
+
+export default function WeatherInfo(props){
+    return (
+      <div className="WeatherInfo">
+        <div className="last-update">
+          Last updated: <FormattedDate date={props.data.date} />{" "}
+        </div>
+        <div className="row">
+          <div className="col-9 currently">
+            <h2 className="city">
+              {props.data.city}{" "}
+              <span className="country">{props.data.country}</span>
+            </h2>
+          </div>
+          <div className="col-3 temp-conversion">
+            <a href="/" className="celsius-conv">
+              °C
+            </a>{" "}
+            |{" "}
+            <a href="/" className="fahrenheit-conv">
+              °F
+            </a>{" "}
+          </div>
+        </div>
+        <div className="current-weather">
+          <div className="row">
+            <div className="col">
+              <img className="weather-icon" src={props.data.icon} alt="" />
+            </div>
+            <div className="col">
+              <ul>
+                <li className="current-temp">
+                  <span className="temperature">
+                    {Math.round(props.data.temperature)}
+                  </span>
+                  <span className="unit">°C</span>
+                </li>
+                <li className="current-description">
+                  {props.data.description}
+                </li>
+                <li className="feels-like">
+                  Feels like {Math.round(props.data.feelsLike)}°C
+                </li>
+                <li className="max-min-temp">
+                  <img className="max-temp-img" src={maxtemp} alt="" />{" "}
+                  <span className="current-max-temp">
+                    {Math.round(props.data.maxTemp)}
+                  </span>
+                  <span className="unit">°C</span>{" "}
+                  <img className="min-temp-img" src={mintemp} alt="" />{" "}
+                  <span className="current-min-temp">
+                    {Math.round(props.data.minTemp)}
+                  </span>
+                  <span className="unit">°C</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="conditions">
+            <ul>
+              <li className="wind">
+                <img className="wind-img" src={wind} alt="" /> Wind:{" "}
+                {Math.round(props.data.wind)} km/h
+              </li>
+              <li className="humidity">
+                {" "}
+                <img className="humidity-img" src={humidity} alt="" />
+                Humidity: {props.data.humidity}%
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
